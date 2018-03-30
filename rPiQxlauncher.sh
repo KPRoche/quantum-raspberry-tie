47e82c9b94909512a37c28ddc3e3e3959a68e99b
Kevin Roche <kevin@twistedimage.com>
2018-03-29 17:02:44 -0700

    Update rPiQxlauncher.sh

--- rPiQxlauncher.sh
+++ rPiQxlauncher.sh
@@ -9,7 +9,7 @@ cd home/pi/YOURCODEDIRECTORY
 # Launch the program
 # (Modify the comments in the lines below to select the program you want to launch at boot)
 # Notes: These are set up to run in the Python 3 IDLE environment.
-# if you prefer to run in a Python 3 terminal, replace idle3 with python3
+# if you prefer to run in a Python 3 terminal, replace 'idle3 -r' with 'python3'
 # the trailing code specifies to run as default user pi, so the python environment
 # has all the appropriate libraries installed.
 # if you have renamed the default account to something other than "pi" update the line appropriately.
